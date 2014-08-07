3c1eb6558790583bab41bc08f48dd94173249f33
thomas <thomas@uschi.office.webvariants.de>
2014-08-07 12:33:23 +0200

    [Config] added tests;

--- test/config/config_test.cpp
+++ test/config/config_test.cpp
@@ -13,6 +13,7 @@ protected:
 	}
 };
 
+
 TEST_F(ConfigTest, Contruct) {
 	Susi::Util::Any cfg = Susi::Util::Any::Object{
 		{"foo","bar"}
@@ -22,21 +23,32 @@ TEST_F(ConfigTest, Contruct) {
 	EXPECT_NO_THROW({
 		auto config = std::make_shared<Susi::Config>("./configtest/config.cfg");
 	});
+
+	
+
 	// Test invalid json
-	io.writeFile("./configtest/config.cfg",cfg.toString()+"wrongformat");
+	//io.writeFile("./configtest/config.cfg",cfg.toString()+"wrongformat");
+	io.writeFile("./configtest/config.cfg","{\"foo\",\"bar\", 22");
 	EXPECT_THROW({
 		auto config = std::make_shared<Susi::Config>("./configtest/config.cfg");
 	},std::runtime_error);
+
+	
 	// Test valid json which is no object;
 	io.writeFile("./configtest/config.cfg","\"wrongformat\"");
 	EXPECT_THROW({
 		auto config = std::make_shared<Susi::Config>("./configtest/config.cfg");
 	},std::runtime_error);
+
+
 	// Test nonexistent file;
 	EXPECT_THROW({
 		auto config = std::make_shared<Susi::Config>("./configtest/wrongname.cfg");
 	},std::runtime_error);
+
 }
+
+
 
 TEST_F(ConfigTest, Get){
 	using Susi::Util::Any;
@@ -44,44 +56,59 @@ TEST_F(ConfigTest, Get){
 	io.writeFile("./configtest/config.cfg",cfg.toString());
 	EXPECT_NO_THROW({
 		auto config = std::make_shared<Susi::Config>("./configtest/config.cfg");
-		Any value = config.get("");
-		EXPECT_EQ(cfg,value);
-		Any value = config.get("foo");
-		EXPECT_EQ(cfg["foo"],value);
-		Any value = config.get("foo.bar");
-		EXPECT_EQ(cfg["foo"]["bar"],value);
-		Any value = config.get("foo.bar.baz");
-		EXPECT_EQ(cfg["foo"]["bar"]["baz"],value);
+		Any value1 = config->get("");
+		EXPECT_EQ(cfg,value1);
+		Any value2 = config->get("foo");		
+		EXPECT_EQ(cfg["foo"],value2);	
+		Any value3 = config->get("foo.bar");
+		EXPECT_EQ(cfg["foo"]["bar"],value3);
+		Any value4 = config->get("foo.bar.baz");
+		EXPECT_EQ(cfg["foo"]["bar"]["baz"],value4);
 	});
+	
 	EXPECT_THROW({
 		auto config = std::make_shared<Susi::Config>("./configtest/config.cfg");
-		config.get("bla");
+		config->get("bla");
 	},std::runtime_error);
+
 	EXPECT_THROW({
 		auto config = std::make_shared<Susi::Config>("./configtest/config.cfg");
-		config.get("bla.blub");
+		config->get("bla.blub");
 	},std::runtime_error);
+
 }
+
 
 TEST_F(ConfigTest,CommandLine){
 	using Susi::Util::Any;
 	Any cfg = Any::Object{{"foo",Any::Object{{"bar", Any::Object{{"baz",123}}}}}};
 	io.writeFile("./configtest/config.cfg",cfg.toString());
+
+	char * cmdLine_1[] = {"prognameIsAllwaysFirstParam","-baz","321"};
+	char * cmdLine_2[] = {"prognameIsAllwaysFirstParam","-baz","321.123"};
+	char * cmdLine_3[] = {"prognameIsAllwaysFirstParam","-baz","this is it"};
+
 	EXPECT_NO_THROW({
 		auto config = std::make_shared<Susi::Config>("./configtest/config.cfg");
-		config.registerCommandLineOption("baz","foo.bar.baz");
-		char ** cmdLine = {"prognameIsAllwaysFirstParam","-baz",321}
-		config.parseCommandLine(3,cmdLine);
-		EXPECT_EQ(Any{321},config.get("foo.bar.baz"));
-		char ** cmdLine = {"prognameIsAllwaysFirstParam","-baz",321.123}
-		config.parseCommandLine(3,cmdLine);
-		EXPECT_EQ(Any{321.123},config.get("foo.bar.baz"));
-		char ** cmdLine = {"prognameIsAllwaysFirstParam","-baz","this is it"}
-		config.parseCommandLine(3,cmdLine);
-		EXPECT_EQ(Any{"this is it"},config.get("foo.bar.baz"));
+		config->registerCommandLineOption("baz","foo.bar.baz");		
+		config->parseCommandLine(3,cmdLine_1);		
+		EXPECT_EQ(Any{321}.toString(),config->get("foo.bar.baz").toString());
+
+				
+		config->parseCommandLine(3,cmdLine_2);
+		EXPECT_EQ(Any{321.123}.toString(),config->get("foo.bar.baz").toString());
+		
+
+		config->parseCommandLine(3,cmdLine_3);
+		EXPECT_EQ(Any{"this is it"}.toString(),config->get("foo.bar.baz").toString());
+		
 	});
+
+	/*
 	EXPECT_THROW({
 		auto config = std::make_shared<Susi::Config>("./configtest/config.cfg");
 		char ** cmdLine = {"prognameIsAllwaysFirstParam","-baz",321}
 	},std::runtime_error);
+
+	*/
 }
