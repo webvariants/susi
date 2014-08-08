4b0b9454e41fb89be32c653f986c8b2b416ec8f9
Tino Rusch <tino.rusch@webvariants.de>
2014-08-08 13:55:40 +0200

    [apiserver] added basic apiserver implementation;

--- src/headers/apiserver/ApiServer.h
+++ src/headers/apiserver/ApiServer.h
@@ -9,25 +9,40 @@
  * @author: Tino Rusch (tino.rusch@webvariants.de)
  */
 
+#ifndef __APISERVER__
+#define __APISERVER__
+
 #include <memory>
-#include "apiserver/JSONServer.h"
+#include "apiserver/JSONTCPServer.h"
+#include "events/global.h"
 
 namespace Susi {
 namespace Api {
 
-class ApiServer : JSONServer {
+class ApiServer : JSONTCPServer {
+public:
+	ApiServer(unsigned short port) : JSONTCPServer{port} {}
 protected:
 	
+	std::map<std::string,std::map<std::string,long>> subscriptions;
+	std::map<std::string,std::map<long,Susi::Events::EventPtr>> eventsToAck;
+
 	virtual void onMessage(std::string & id, Susi::Util::Any & packet) override;
 	
 	void handleRegisterConsumer(std::string & id, Susi::Util::Any & packet);
 	void handleRegisterProcessor(std::string & id, Susi::Util::Any & packet);
 	void handleUnregister(std::string & id, Susi::Util::Any & packet);
 	void handlePublish(std::string & id, Susi::Util::Any & packet);
+	void handleAck(std::string & id, Susi::Util::Any & packet);
+
+	void handleMalformedPacket(std::string & id, Susi::Util::Any & packet);
 	
+	void sendOk(std::string & id);
+	void sendFail(std::string & id);
+
 };
 
 }
 }
 
-
+#endif // __APISERVER__
