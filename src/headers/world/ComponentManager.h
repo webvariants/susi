03cb9b2b97a8fb43b113fd622e4227102d5d246a
Tino Rusch <tino.rusch@webvariants.de>
2014-08-30 09:07:22 +0200

    [component manager] splitted into .cpp and .h file;

--- src/headers/world/ComponentManager.h
+++ src/headers/world/ComponentManager.h
@@ -36,68 +36,12 @@ namespace System {
 
 		ComponentManager(Susi::Util::Any::Object config) : config{config} {}
 
-		void registerComponent(std::string name, RegisterFunction func){
-			registerFunctions.emplace(name,std::move(func));
-		}
-		void registerDependency(std::string subject, std::string dependency){
-			auto & deps = dependencies[subject];
-			deps.push_back(dependency);
-			auto & iDeps = inverseDependencies[dependency];
-			iDeps.push_back(subject);
-		}
-
-		bool loadComponent(std::string name){
-			if(components.find(name) == components.end() && registerFunctions[name]){
-				ComponentData data;
-				if(config[name].isNull())return false;
-				data.component = registerFunctions[name](config[name]);
-				components[name] = data;
-				return true;
-			}
-			return false;
-		}
-
-		bool unloadComponent(std::string name){
-			if(components.find(name) == components.end()){
-				return false;
-			}
-			components[name].component->stop();
-			components.erase(name);
-			return true;
-		}
-
-		bool startComponent(std::string name){
-			if(components.find(name) == components.end() && !loadComponent(name)){
-				return false;
-			}
-			auto & data = components[name];
-			if(data.running){
-				return false;
-			}
-			for(std::string & dep : dependencies[name]){
-				startComponent(dep);
-				if(components.find(dep) == components.end() || !components[dep].running){
-					return false;
-				}
-			}
-			data.component->start();
-			data.running = true;
-
-			return true;
-		}
-		
-		bool stopComponent(std::string name){
-			if(components.find(name) == components.end() || components[name].running == false) {
-				return false;
-			}
-			for(std::string & iDep : inverseDependencies[name]){
-				stopComponent(iDep);
-			}
-			components[name].component->stop();
-			components[name].running = false;
-
-			return true;
-		}
+		void registerComponent(std::string name, RegisterFunction func);
+		void registerDependency(std::string subject, std::string dependency);
+		bool loadComponent(std::string name);
+		bool unloadComponent(std::string name);
+		bool startComponent(std::string name);
+		bool stopComponent(std::string name);
 	};
 }
 
---
+++ src/sources/world/ComponentManager.cpp
@@ -0,0 +1,64 @@
+#include "world/ComponentManager.h"
+
+void Susi::System::ComponentManager::registerComponent(std::string name, RegisterFunction func){
+	registerFunctions.emplace(name,std::move(func));
+}
+void Susi::System::ComponentManager::registerDependency(std::string subject, std::string dependency){
+	auto & deps = dependencies[subject];
+	deps.push_back(dependency);
+	auto & iDeps = inverseDependencies[dependency];
+	iDeps.push_back(subject);
+}
+
+bool Susi::System::ComponentManager::loadComponent(std::string name){
+	if(components.find(name) == components.end() && registerFunctions[name]){
+		ComponentData data;
+		if(config[name].isNull())return false;
+		data.component = registerFunctions[name](config[name]);
+		components[name] = data;
+		return true;
+	}
+	return false;
+}
+
+bool Susi::System::ComponentManager::unloadComponent(std::string name){
+	if(components.find(name) == components.end()){
+		return false;
+	}
+	components[name].component->stop();
+	components.erase(name);
+	return true;
+}
+
+bool Susi::System::ComponentManager::startComponent(std::string name){
+	if(components.find(name) == components.end() && !loadComponent(name)){
+		return false;
+	}
+	auto & data = components[name];
+	if(data.running){
+		return false;
+	}
+	for(std::string & dep : dependencies[name]){
+		startComponent(dep);
+		if(components.find(dep) == components.end() || !components[dep].running){
+			return false;
+		}
+	}
+	data.component->start();
+	data.running = true;
+
+	return true;
+}
+
+bool Susi::System::ComponentManager::stopComponent(std::string name){
+	if(components.find(name) == components.end() || components[name].running == false) {
+		return false;
+	}
+	for(std::string & iDep : inverseDependencies[name]){
+		stopComponent(iDep);
+	}
+	components[name].component->stop();
+	components[name].running = false;
+
+	return true;
+}
