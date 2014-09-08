b99ab7b5bf2d0d34652f462991221f717791f301
thomas <thomas@uschi.office.webvariants.de>
2014-09-08 12:31:28 +0200

    [ApiServerComponent] started test;

---
+++ test/apiserver/ApiServerComponent_test.cpp
@@ -0,0 +1,17 @@
+#include "gtest/gtest.h"
+#include "ApiServerForComponent.h"
+
+
+class ApiServerForComponentTest : public ::testing::Test {
+protected:
+	
+	virtual void SetUp() override {
+		
+	}
+	virtual void TearDown() override {
+		
+	}
+};
+
+TEST_F(ApiServerForComponentTest, Contruct) {
+}
