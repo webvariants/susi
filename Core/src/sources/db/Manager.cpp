7b44873fca035448565744cdd7a06664071ad385
Tino Rusch <tino.rusch@webvariants.de>
2014-09-11 10:56:04 +0200

    [dbmanager] use new config structure;

--- Core/src/sources/db/Manager.cpp
+++ Core/src/sources/db/Manager.cpp
@@ -27,9 +27,9 @@ void Susi::DB::Manager::init(Susi::Util::Any config) {
 			try{
 				Susi::Util::Any entry = config[i];
 				std::cout<<"INIT:"<<entry.toString()<<std::endl;
-				std::string identifier = entry[0]; // id
-				std::string dbtype     = entry[1]; // type
-				std::string connectURI = entry[2]; // uri
+				std::string identifier = entry["id"]; // id
+				std::string dbtype     = entry["type"]; // type
+				std::string connectURI = entry["uri"]; // uri
 				addDatabase(identifier, dbtype, connectURI);
 			}catch(const std::exception & e){
 				Susi::Logger::error("malformed config file");
