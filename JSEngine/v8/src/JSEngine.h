3b3c8fea34f1861d0a08ef817a588a6e41265850
Christian <christian.sonderfeld@webvariants.de>
2014-09-11 17:13:50 +0200

    added publish

--- JSEngine/v8/src/JSEngine.h
+++ JSEngine/v8/src/JSEngine.h
@@ -19,6 +19,7 @@
 #include <fstream>
 #include <streambuf>
 
+#include "apiserver/ApiClient.h"
 #include "logger/Logger.h"
 #include "util/Any.h"
 
@@ -29,58 +30,65 @@ namespace V8 {
 using namespace v8;
 
 class Engine{
-static Susi::Util::Any fromJS(Handle<Value> value) {
-	HandleScope scope;
-
-	Handle<Context> context = Context::GetCurrent();
-    Handle<Object> global = context->Global();
-
-	Handle<Object> JSON = global->Get(String::New("JSON"))->ToObject();
-    Handle<Function> JSON_stringify = Handle<Function>::Cast(JSON->Get(String::New("stringify")));
-
-    Handle<Value> stringifiedValue = JSON_stringify->Call(JSON, 1, &value);
-    if(stringifiedValue->IsString()) {
-    	String::Utf8Value stringValue{stringifiedValue};
-    	return Susi::Util::Any::fromString(std::string(*stringValue));
-    }
-    return Susi::Util::Any{};
-}
-
-static Handle<Value> fromCPP(Susi::Util::Any value) {
-	HandleScope scope;
-
-	Handle<Context> context = Context::GetCurrent();
-    Handle<Object> global = context->Global();
-
-	Handle<Object> JSON = global->Get(String::New("JSON"))->ToObject();
-    Handle<Function> JSON_parse = Handle<Function>::Cast(JSON->Get(String::New("parse")));
-
-    Handle<Value> valueHandle{String::New(value.toString().c_str())};
-
-    return scope.Close(JSON_parse->Call(JSON, 1, &valueHandle));
-}
-
-static Handle<Value> Log(const Arguments& args) {
-	Susi::Util::Any cppvalue = fromJS(args[0]);
-	Handle<Value> jsvalue = fromCPP(cppvalue);
-	String::Utf8Value str(jsvalue);
-	Susi::Util::Any cppvalue_2 = fromJS(jsvalue);
-	Susi::Logger::log("From JS to CPP: "+cppvalue.toString());
-	Susi::Logger::log("Back to JS: "+ std::string(*str));
-	Susi::Logger::log("From JS to CPP again: "+cppvalue_2.toString());
-	Susi::Logger::log("ALL IN ONE: "+fromJS(fromCPP(fromJS(args[0]))).toString());
-
-	return Undefined();
-}
-
 protected:
-	 // Executes a string within the current v8 context.
+	static Persistent<Context> setupContext() {
+		// Create a template for the global object.
+		Handle<ObjectTemplate> global{ObjectTemplate::New()};
+		// Bind the global 'print' function to the C++ Print callback.
+		global->Set(String::New("log"), FunctionTemplate::New(Susi::JS::V8::Engine::Log));
+		global->Set(String::New("publish"), FunctionTemplate::New(Susi::JS::V8::Engine::Publish));
+
+		return Context::New(NULL, global);
+	}
+
+	static Susi::Util::Any fromJS(Handle<Value> value) {
+		HandleScope scope;
+
+	    Handle<Object> global = context->Global();
+
+		Handle<Object> JSON = global->Get(String::New("JSON"))->ToObject();
+	    Handle<Function> JSON_stringify = Handle<Function>::Cast(JSON->Get(String::New("stringify")));
+
+	    Handle<Value> stringifiedValue = JSON_stringify->Call(JSON, 1, &value);
+	    if(stringifiedValue->IsString()) {
+	    	String::Utf8Value stringValue{stringifiedValue};
+	    	return Susi::Util::Any::fromString(std::string(*stringValue));
+	    }
+	    return Susi::Util::Any{};
+	}
+
+	static Handle<Value> fromCPP(Susi::Util::Any value) {
+		HandleScope scope;
+
+		Handle<Context> context = Context::GetCurrent();
+	    Handle<Object> global = context->Global();
+
+		Handle<Object> JSON = global->Get(String::New("JSON"))->ToObject();
+	    Handle<Function> JSON_parse = Handle<Function>::Cast(JSON->Get(String::New("parse")));
+
+	    Handle<Value> valueHandle{String::New(value.toString().c_str())};
+
+	    return scope.Close(JSON_parse->Call(JSON, 1, &valueHandle));
+	}
+
+	// Susi API Mapping
+
+	static Handle<Value> Log(const Arguments& args) {
+		Susi::Logger::log(fromJS(args[0]).toString());
+		return Undefined();
+	}
+
+	static Handle<Value> Publish(const Arguments& args) {
+		susi_api->publish(susi_api->createEvent(fromJS(args[0])));
+		return Undefined();
+	}
+
+	// Executes a string within the current v8 context.
 	bool run(Handle<String> source) {
 		TryCatch try_catch;
 		bool return_val = false;
-		Persistent<Context> context{this->setupContext()};
 		if (context.IsEmpty()) {
-			Susi::Logger::log("JS::Engine.run: Error creating context");
+			Susi::Logger::log("JS::Engine.run: context is Empty");
 			return_val = false;
 	    }
 	    else {
@@ -88,6 +96,7 @@ protected:
 			Context::Scope context_scope{context};
 			Handle<Script> script = Script::New(source, String::New("script"));
 			if (script.IsEmpty()) {
+				Susi::Logger::log("JS::Engine.run: Script is Empty");
 				return_val = false;
 			} else {
 				Handle<Value> result = script->Run();
@@ -99,33 +108,39 @@ protected:
 			    }
 			}
 			context->Exit();
-    		context.Dispose();
 	    }
 
 		return return_val;
 	}
 
-	Persistent<Context> setupContext() {
-		// Create a template for the global object.
-		Handle<ObjectTemplate> global{ObjectTemplate::New()};
-		// Bind the global 'print' function to the C++ Print callback.
-		global->Set(String::New("log"), FunctionTemplate::New(Susi::JS::V8::Engine::Log));
+public:
+	HandleScope scope;
+	static Persistent<Context> context;
+	static std::shared_ptr<Susi::Api::ApiClient> susi_api;
 
-		return Context::New(NULL, global);
+	Engine()
+	{
+		if(context.IsEmpty()) {
+			Locker v8Locker;
+			HandleScope scope;
+			context = Persistent<Context>(setupContext());
+			susi_api = std::shared_ptr<Susi::Api::ApiClient>{new Susi::Api::ApiClient("[::1]:4000")};
+		}
 	}
 
-public:
-	Engine(){}
-
 	bool run(std::string source) {
-		HandleScope handle_scope;
+		Locker v8Locker;
+		HandleScope scope;
 		return this->run(String::New(source.c_str()));
 	}
 
-	~Engine() {}
+	~Engine() {
+		context.Dispose();
+	}
 };
 
-
+Persistent<Context> Engine::context = Persistent<Context>();
+std::shared_ptr<Susi::Api::ApiClient> Engine::susi_api = nullptr;
 }
 }
 }
