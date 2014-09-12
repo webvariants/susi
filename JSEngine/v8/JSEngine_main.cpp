18bb6070ac279b9b0ca6887e071d8e50768da727
Christian <christian.sonderfeld@webvariants.de>
2014-09-12 20:29:37 +0200

    2nd iteration basic coding done; NOT TESTED!

--- JSEngine/v8/JSEngine_main.cpp
+++ JSEngine/v8/JSEngine_main.cpp
@@ -6,19 +6,25 @@
 
 int main(int argc, char** argv){
 	if(argc < 2){
-		std::cout<<"usage : "<<argv[0]<<" <susi address> <source>"<<std::endl;
-		std::cout<<"sample: "<<argv[0]<<" source"<<std::endl;
+		std::cout<<"usage : "<<argv[0]<<" <sourcefile>"<<std::endl;
+		std::cout<<"sample: "<<argv[0]<<" sourcefile "<<std::endl;
 		return 1;
 	}
 
-	Susi::JS::V8::Engine engine{};
+	std::shared_ptr<Susi::Util::Channel<Susi::JS::V8::EngineToBridgeMessage>> bridge_in{new Susi::Util::Channel<Susi::JS::V8::EngineToBridgeMessage>()};
+	std::shared_ptr<Susi::Util::Channel<Susi::JS::V8::BridgeToEngineMessage>> engine_in{new Susi::Util::Channel<Susi::JS::V8::BridgeToEngineMessage>()};
 
-	engine.run(std::string{argv[1]});
+	Susi::JS::V8::Engine engine{engine_in, bridge_in};
+	Susi::JS::V8::EngineBridge enginebridge{bridge_in, engine_in};
+
+	enginebridge.start();
+	engine.runFile(std::string{argv[1]});
 
 	bool stop = false;
 	std::mutex mutex;
 	std::condition_variable stop_cond;
 	std::unique_lock<std::mutex> lock{mutex};
-	stop_cond.wait_for(lock, std::chrono::seconds{2},[&stop](){return true;});
+	stop_cond.wait_for(lock, std::chrono::seconds{5},[&stop](){return false;});
 
 }
+
