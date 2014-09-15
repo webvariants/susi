fbceeb5a5d354c3d33d5ea7878e39dee7fb6b6ce
Tino Rusch <tino.rusch@webvariants.de>
2014-09-15 10:17:02 +0200

    [testing] added base component test and first sample component test (heartbeat);

---
+++ Core/test/heartbeat/HeartBeatComponentTest.cpp
@@ -0,0 +1,41 @@
+#include "util/ComponentTest.h"
+
+class HeartBeatComponentTest : public ComponentTest {
+protected:
+	
+	virtual void SetUp() override {
+		componentManager->startComponent("heartbeat");
+	}
+	
+	virtual void TearDown() override {
+		componentManager->stopComponent("heartbeat");
+	}
+
+	virtual void GoodCases() override {
+		auto sharedEvent = waitForConsumerEvent("heartbeat::one",std::chrono::milliseconds{1100});
+		EXPECT_TRUE(sharedEvent.get()!=nullptr);
+	}
+
+	virtual void BadCases() override {
+		auto event = waitForConsumerEvent("heartbeat::wrong",std::chrono::milliseconds{1100});
+		EXPECT_TRUE(event.get()==nullptr);
+	}
+
+	virtual void EdgeCases() override {
+		auto event = waitForProcessorEvent("heartbeat::one",std::chrono::milliseconds{1100});
+		EXPECT_TRUE(event.get()!=nullptr);
+	}
+
+};
+
+TEST_F(HeartBeatComponentTest,GoodCases){
+	GoodCases();
+}
+
+TEST_F(HeartBeatComponentTest,BadCases){
+	BadCases();
+}
+
+TEST_F(HeartBeatComponentTest,EdgeCases){
+	EdgeCases();
+}
