77891c5feb580c3094a2c92910b11759d865597a
thomas <thomas@uschi.office.webvariants.de>
2014-09-15 15:56:44 +0200

    [Auth] removed old event test;

--- Core/test/auth/AuthControllerComponentTest.cpp
+++ Core/test/auth/AuthControllerComponentTest.cpp
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2014, webvariants GmbH, http://www.webvariants.de
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * @author: Thomas Krause (thomas.krause@webvariants.de)
+ */
+
 #include "util/ComponentTest.h"
 
 class AuthControllerComponentTest : public ComponentTest {
