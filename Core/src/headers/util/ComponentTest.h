be380469ee19f7cd175086a1ea470cad42640d9f
Tino Rusch <tino.rusch@webvariants.de>
2014-09-22 08:44:10 +0200

    [testing] fixed engine-starter and httpserver tests;

--- Core/src/headers/util/ComponentTest.h
+++ Core/src/headers/util/ComponentTest.h
@@ -24,9 +24,9 @@ public:
 	std::string base_path;
 
 	ComponentTest(){
-		Susi::Logger::setLevel(7);
+		Susi::Logger::setLevel(0);
 
-		base_path = Poco::Path(Poco::Path::current()).toString() + "component_test/";
+		base_path = Poco::Path(Poco::Path::current()).toString() + "component_test";
 
 		Susi::Util::Any::Object cfg = Susi::Util::Any::fromString(getConfigString(base_path));
 		componentManager = Susi::System::createSusiComponentManager(cfg);
@@ -36,7 +36,7 @@ public:
 		io.makeDir(base_path);
 	}
 
-	~ComponentTest(){
+	virtual ~ComponentTest(){
 		io.deletePath(base_path);
 	}
 
@@ -101,7 +101,7 @@ protected:
 				"},"
 				"\"httpserver\": {"
 					"\"address\": \"[::1]:8080\","
-					"\"assets\": \""+path+"/tmp/assets\""
+					"\"assets\": \""+path+"/assets\""
 				"},"
 				"\"apiserver\": {}"
 			"}";
