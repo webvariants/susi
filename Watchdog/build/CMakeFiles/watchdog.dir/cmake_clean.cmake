0748b99f621cd4b39b33c9b514937b73b5d80173
thomas <thomas@uschi.office.webvariants.de>
2014-09-26 11:45:20 +0200

    [Cmake] started;

---
+++ Watchdog/build/CMakeFiles/watchdog.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/watchdog.dir/src/sources/main.cc.o"
+  "watchdog.pdb"
+  "watchdog"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/watchdog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ Watchdog/src/sources/main.cc
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2014, webvariants GmbH, http://www.webvariants.de
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * @author: Thomas Krause (thomas.krause@webvariants.de)
+ */
+
+#include <iostream>
+#include <mutex>
+#include <chrono>
+#include <condition_variable>
+
+#include "headers/Watchdog.h"
+
+std::condition_variable waitCond;
+
+void waitForEver(){
+	std::mutex mutex;
+	std::unique_lock<std::mutex> lk(mutex);
+	waitCond.wait(lk,[](){return false;});
+}
+
+
+void signalHandler (int signum) {
+	std::cout << "Interrupt signal (" << signum << ") received.\n";
+	componentManager->stopAll();
+	exit(0);
+}
+
+int main(int argc, char** argv){
+
+	// register signal SIGINT and signal handler  
+    signal(SIGINT, signalHandler); 
+
+	waitForEver();
+
+	exit(0)
+}
