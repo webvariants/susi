a33a2201d749eaa6cb130d5e4f24d5ce119097b9
Tino Rusch <tino.rusch@webvariants.de>
2014-10-06 10:23:53 +0200

    [release] added new binaries to package build script; [release] added 0.1.5 release package;

--- Scripts/build_debian_packages.sh
+++ Scripts/build_debian_packages.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-pushd Core/build
-#rm -rvf *
-cmake ..
-make -j4
-popd
-
-pushd JSEngine/v8/build
-#rm -rvf *
-cmake ..
-make -j4
-popd
+bash Scripts/rebuild_all.sh hard
 
 mkdir -p susi_package/DEBIAN
 mkdir -p susi_package/usr/bin
@@ -24,14 +14,25 @@ Priority: optional
 Section: devel
 Maintainer: Tino Rusch <tino.rusch@webvariants.de>
 Architecture: all
-Version: 0.1.4a
+Version: 0.1.5
 Description: The SUSI server
 EOF
 
+#copy core
 cp Core/build/libsusi.so susi_package/usr/lib/
 cp Core/build/susi susi_package/usr/bin/
 cp Core/config.json susi_package/etc/susi/
 
+#copy cpp engine lib
+cp CPPEngine/build/libsusicpp.so susi_package/usr/lib/
+
+#copy debugger
+cp Debugger/build/susi-debugger susi_package/usr/bin/
+
+#copy watchdog
+cp Watchdog/build/watchdog susi_package/usr/bin/
+
+#copy third party libs
 cp /usr/local/lib/libPocoFoundation.so.23 susi_package/usr/lib/
 cp /usr/local/lib/libPocoUtil.so.23 susi_package/usr/lib/
 cp /usr/local/lib/libPocoJSON.so.23 susi_package/usr/lib/
