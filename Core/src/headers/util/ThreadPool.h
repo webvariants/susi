6949442cf70819dfd9d1034f32805c1b200430f8
Tino Rusch <tino.rusch@webvariants.de>
2014-10-09 10:28:33 +0200

    Revert "[gcc-4.7 support] refactored ThreadPool, fixed DB;"
    This reverts commit 68fb7ef8d53b9ce9d196dcd2edd7e62e8fb8d860.

--- Core/src/headers/util/ThreadPool.h
+++ Core/src/headers/util/ThreadPool.h
@@ -12,8 +12,7 @@
 #ifndef __THREADPOOL__
 #define __THREADPOOL__
 
-#include <Poco/Runnable.h>
-#include <Poco/ThreadPool.h>
+#include <thread>
 #include <functional>
 #include <vector>
 #include "util/Channel.h"
@@ -25,51 +24,22 @@ namespace Susi {
 
         class ThreadPool {
         protected:
-            Poco::ThreadPool pool;
-            
-            class Work : public Poco::Runnable {
-            protected:
-                ThreadPool * pool;
+            struct Work {
                 std::function<void()>               work;
-                std::function<void( std::string )>  error;
-            public:
-                Work(ThreadPool * _pool, std::function<void()> _work, std::function<void( std::string )> _error) 
-                    : pool{_pool}, work{_work}, error{_error} {}
-                virtual void run() override {
-                    try{
-                        work();
-                    }catch(const std::exception & e){
-                        try{
-                            error(e.what());
-                        }catch(...){}
-                    }
-                    std::lock_guard<std::mutex> lock{pool->mutex};
-                    for(auto it=pool->workers.begin(); it!=pool->workers.end(); ++it){
-                        if(&(*it) == this){
-                            pool->workers.erase(it);
-                            return;
-                        }
-                    }
-                }
+                std::function<void( std::string )>    error;
             };
 
-            std::mutex mutex;
-            std::vector<Work> workers;
+            Channel<Work> _workChannel;
+            std::vector<std::thread> _threads;
+
+            void startThread();
 
         public:
             ThreadPool() : ThreadPool {4,16} {};
-            ThreadPool( size_t workers, size_t buffsize ){}
+            ThreadPool( size_t workers, size_t buffsize );
             void add( std::function<void()> work,
-                      std::function<void( std::string )> error = std::function<void( std::string )> {} ){
-            
-                std::lock_guard<std::mutex> lock{mutex};
-                Work worker{this,std::move(work),std::move(error)};
-                workers.push_back(std::move(worker));               
-                pool.start(workers[workers.size()-1]);
-            }
-            ~ThreadPool(){
-                pool.stopAll();
-            }
+                      std::function<void( std::string )> error = std::function<void( std::string )> {} );
+            ~ThreadPool();
         };
 
     } //ns::Util
