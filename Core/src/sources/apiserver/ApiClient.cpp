832261c30ddbecb72acc745634f172da2647bdcf
Tino Rusch <tino.rusch@webvariants.de>
2014-11-10 14:35:16 +0100

    [apiclient] added reconnect handling;

--- Core/src/sources/apiserver/ApiClient.cpp
+++ Core/src/sources/apiserver/ApiClient.cpp
@@ -1,21 +1,23 @@
 #include "apiserver/ApiClient.h"
 
 void Susi::Api::ApiClient::publish( Susi::Events::EventPtr event, Susi::Events::Consumer finishCallback ) {
-    finishCallbacks[event->getID()] = finishCallback;
     sendPublish( *event );
-    delete event.release();
+    publishData[event->getID()] = PublishData{std::move(event),finishCallback};
 }
 
 long Susi::Api::ApiClient::subscribe( std::string topic, Susi::Events::Processor processor , std::string name) {
     sendRegisterProcessor( topic,name );
-    return Susi::Events::Manager::subscribe( topic,processor,name );
+    long id = Susi::Events::Manager::subscribe( topic,processor,name );
+    subscribes[id] = SubscribeData{topic,name,true};
+    return id;
 }
 
 long Susi::Api::ApiClient::subscribe( std::string topic, Susi::Events::Consumer consumer , std::string name) {
     sendRegisterConsumer( topic,name );
-    return Susi::Events::Manager::subscribe( topic,consumer,name );
+    long id = Susi::Events::Manager::subscribe( topic,consumer,name );
+    subscribes[id] = SubscribeData{topic,name,false};
+    return id;
 }
-
 
 void Susi::Api::ApiClient::onConsumerEvent( Susi::Events::Event & event ) {
     auto evt = createEvent( event.getTopic() );
@@ -27,16 +29,16 @@ void Susi::Api::ApiClient::onProcessorEvent( Susi::Events::Event & event ) {
     auto evt = createEvent( event.getTopic() );
     *evt = event;
     Susi::Events::Manager::publish( std::move( evt ),[this]( Susi::Events::SharedEventPtr event ) {
-        std::cout<<"finished in client, acking to susi!"<<std::endl;
         sendAck( *event );
-        std::cout<<"sended ack"<<std::endl;
     } );
 }
 
 void Susi::Api::ApiClient::onAck( Susi::Events::Event & event ) {
     auto evt = std::make_shared<Susi::Events::Event>( event );
-    auto callback = finishCallbacks[evt->getID()];
-    if( callback ) {
-        callback( evt );
+    auto & data = publishData[evt->getID()];
+    delete data.event.release();
+    if( data.finishCallback ) {
+        data.finishCallback( evt );
     }
+    publishData.erase(evt->getID());
 }
