d933a3a2bfde5c1321870458b9e3cfe5a3a899c6
Tino Rusch <tino.rusch@webvariants.de>
2014-11-24 15:06:48 +0100

    [build] refactored build;

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,6 @@ module.exports = function(grunt) {
         options: {
           create: [
             '<%= pkg.project.directories.build %>/core',
-            '<%= pkg.project.directories.build %>/cppengine',
             '<%= pkg.project.directories.build %>/watchdog',
             '<%= pkg.project.directories.build %>/jsengine'
           ]
@@ -30,15 +29,6 @@ module.exports = function(grunt) {
           stderr: false,
           execOptions: {
             cwd: '<%= pkg.project.directories.build %>/core'
-          }
-        }
-      },
-      cmake_cppengine: {
-        command: 'cmake -DPOCO_INCLUDE_DIR=../../node_modules/poco/include -DSOCI_INCLUDE_DIR=../../node_modules/soci/include/soci -DCMAKE_LIBRARY_PATH=../../node_modules/ ../../<%= pkg.project.directories.cppengine %>',
-        options: {
-          stderr: false,
-          execOptions: {
-            cwd: '<%= pkg.project.directories.build %>/cppengine'
           }
         }
       },
@@ -66,15 +56,6 @@ module.exports = function(grunt) {
           stderr: false,
           execOptions: {
             cwd: '<%= pkg.project.directories.build %>/core'
-          }
-        }
-      },
-      make_cppengine: {
-        command: 'make -j4',
-        options: {
-          stderr: false,
-          execOptions: {
-            cwd: '<%= pkg.project.directories.build %>/cppengine'
           }
         }
       },
@@ -123,19 +104,6 @@ module.exports = function(grunt) {
           }
         ]
       },
-      cppengine: {
-        options: {
-          mode: true
-        },
-        files: [
-          {
-            expand: true,
-            flatten: true,
-            src: ['<%= pkg.project.directories.build %>/cppengine/sample'],
-            dest: '<%= pkg.project.directories.bin %>'
-          }
-        ]
-      },
       jsengine: {
         options: {
           mode: true
@@ -179,40 +147,25 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-newer');
   grunt.loadNpmTasks('grunt-debian-package');
 
-  grunt.registerTask('build', 'generate a clean build of everything', [
-    'clean:bin',
-    'clean:build',
-    'mkdir:all',
-    'shell:cmake_core',
-    'shell:make_core',
-    'copy:core',
-    'shell:cmake_cppengine',
-    'shell:make_cppengine',
-    'copy:cppengine',
-    'shell:cmake_watchdog',
-    'shell:make_watchdog',
-    'copy:watchdog'
-  ]);
+  var register = function(name){
+    grunt.registerTask(name,'build '+name,[
+      'mkdir',
+      'shell:cmake_'+name,
+      'shell:make_'+name,
+      'newer:copy:'+name
+    ]);
+  };
 
-  grunt.registerTask('development', 'update all changes', [
-    'mkdir:all',
-    'shell:cmake_core',
-    'shell:make_core',
-    'newer:copy:core',
-    'shell:cmake_cppengine',
-    'shell:make_cppengine',
-    'newer:copy:cppengine',
-    'shell:cmake_watchdog',
-    'shell:make_watchdog',
-    'newer:copy:watchdog'
-  ]);
-
-  grunt.registerTask('core',['mkdir','shell:cmake_core','shell:make_core','newer:copy:core']);
-  grunt.registerTask('jsengine',['core','shell:cmake_jsengine','shell:make_jsengine','newer:copy:jsengine']);
+  register('core');
+  register('jsengine');
+  register('watchdog');
 
   grunt.registerTask('test', 'run the susi tests', ['development','shell:test_core']);
-  grunt.registerTask('dev', ['development']);
-  grunt.registerTask('default', ['development']);
+  
+  grunt.registerTask('development', ['core','jsengine','watchdog']);
+  grunt.registerTask('build', ['clean','core','jsengine','watchdog']);
 
+  grunt.registerTask('dev',['development']);  
+  grunt.registerTask('default', ['development']);
 
 };
