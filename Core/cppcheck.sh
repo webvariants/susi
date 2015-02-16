2220958017b86feba63913ef2c27d0ee4e2cde35
Tino Rusch <tino.rusch@webvariants.de>
2015-02-16 13:53:08 +0100

    [repo] cleanup no.2;

--- Core/cppcheck.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cppcheck --xml -I ./src/headers  -i ./test/gtest-1.7.0 -i ./build --enable=all --max-configs=25 --force ./ 2>cppcheck.xml || exit $?
-cppcheck-htmlreport --file=cppcheck.xml --title=Susi-Core --report-dir=cppcheck_reports --source-dir=. || exit $?
-google-chrome cppcheck_reports/index.html
-
-exit $?
-
