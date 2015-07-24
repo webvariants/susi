763e6aaddee9c72365f72fa4ad71fc36dd0b9d7d
Tino Rusch <tino.rusch@webvariants.de>
2015-07-24 11:11:23 +0200

    [susi-duktape] added javascrip engine on duktape base;

---
+++ susi-duktape/headers/susi/DuktapeEngine.h
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2014, webvariants GmbH, http://www.webvariants.de
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * @author: Tino Rusch (tino.rusch@webvariants.de)
+ */
+
+#ifndef __DUKTAPEENGINE__
+#define __DUKTAPEENGINE__
+
+#include <sstream>
+#include "susi/SusiClient.h"
+#include "susi/duktape.h"
+
+namespace Susi {
+namespace Duktape {
+
+class JSEngine;
+
+extern JSEngine *enginePtr;
+extern std::string susiJS;
+
+class JSEngine {
+  public:
+    JSEngine(Susi::SusiClient & susi, std::string script) :
+        _client{susi},
+        _script{script} {}
+
+    // The start function subscribes to events and attaches its handlers
+    void start();
+
+    // When stopping, all subscriptions are deleted
+    void stop();
+
+  protected:
+    Susi::SusiClient & _client;
+    std::string _script;
+
+    duk_context *ctx;
+    std::mutex mutex;
+
+    enum {
+        OUTPUT = 0,
+        INPUT = 1
+    };
+
+    std::map<std::string, Susi::EventPtr> pendingEvents;
+    std::map<std::string, long> registerIdsConsumer;
+    std::map<std::string, long> registerIdsProcessor;
+
+    static duk_ret_t js_registerConsumer(duk_context *ctx) ;
+    static duk_ret_t js_registerProcessor(duk_context *ctx) ;
+    static duk_ret_t js_publish(duk_context *ctx) ;
+    static duk_ret_t js_ack(duk_context *ctx) ;
+    static duk_ret_t js_dismiss(duk_context *ctx) ;
+    static duk_ret_t js_log(duk_context *ctx) ;
+    static duk_ret_t js_unregisterConsumer(duk_context *ctx) ;
+    static duk_ret_t js_unregisterProcessor(duk_context *ctx) ;
+
+    void registerProcessor(std::string topic);
+    void registerConsumer(std::string topic);
+    void unregisterConsumer(std::string topic);
+    void unregisterProcessor(std::string topic);
+    void publish(std::string eventData);
+    void ack(std::string eventData);
+    void dismiss(std::string eventData);
+
+    void init();
+
+};
+
+
+}
+}
+
+#endif // __DUKTAPEENGINE__
---
+++ susi-duktape/sources/SusiJS.cpp
@@ -0,0 +1,171 @@
+#include "susi/duktape/DuktapeEngine.h"
+
+std::string Susi::Duktape::susiJS = R"SUSIJS(
+
+var susi = {  
+    _consumerCallbacks: [], 
+    _processorCallbacks: [],
+
+    _processorTopicCounter: {},
+    _consumerTopicCounter: {},
+
+    _publishCallbacks: {},
+    _processorProcesses: {},
+
+    _processed: [],
+
+    registerConsumer: function(topic,callback) {
+        var id = this._genID(); 
+        this._consumerCallbacks.push({topic: topic, callback: callback, id: id}); 
+        var count = this._consumerTopicCounter[topic] || 0;
+        count++;
+        this._consumerTopicCounter[topic] = count;
+        if(count == 1){ 
+            _registerConsumer(topic); 
+        } 
+        return id; 
+    },
+
+    registerProcessor: function(topic,callback) { 
+        var id = this._genID(); 
+        this._processorCallbacks.push({topic: topic, callback: callback, id: id}); 
+        var count = this._processorTopicCounter[topic] || 0;
+        count++;
+        this._processorTopicCounter[topic] = count;
+        if(count == 1){ 
+            _registerProcessor(topic);
+        } 
+        return id; 
+    },
+
+    unregisterConsumer: function(id){ 
+        for(var i=0;i<this._consumerCallbacks;i++){
+            if(this._consumerCallbacks[i].id === id){
+                var topic = this._consumerCallbacks[i].topic;
+                this._consumerCallbacks.splice(i,1);
+                var count = this._consumerTopicCounter[topic];
+                count--;
+                if(count === 0){
+                    delete(this._consumerTopicCounter[topic]);
+                    _unregisterConsumer(topic);
+                }else{
+                    this._consumerTopicCounter[topic] = count;
+                }
+                return true;
+            }
+        }
+        return false; 
+    },
+
+    unregisterProcessor: function(id){ 
+        for(var i=0;i<this._processorCallbacks;i++){
+            if(this._processorCallbacks[i].id === id){
+                var topic = this._processorCallbacks[i].topic;
+                this._processorCallbacks.splice(i,1);
+                var count = this._processorTopicCounter[topic];
+                count--;
+                if(count === 0){
+                    delete(this._processorTopicCounter[topic]);
+                    _unregisterProcessor(topic);
+                }else{
+                    this._processorTopicCounter[topic] = count;
+                }
+                return true;
+            }
+        }
+        return false; 
+    },
+
+    publish: function(event,callback) { 
+        if(event.id === undefined){ 
+            event.id = ''+this._genID(); 
+        } 
+        if(callback !== undefined) { 
+            this._publishCallbacks[event.id] = callback; 
+        }
+        _publish(JSON.stringify(event)); 
+    },
+
+    ack: function(event){
+        var process = this._processorProcesses[event.id];
+        if(process.next >= process.processors.length){
+            delete this._processorProcesses[event.id];
+            _ack(JSON.stringify(event)); 
+        }else{
+            process.next++;
+            process.processors[process.next-1](event);
+        }
+    },
+
+    dismiss: function(event){ 
+        delete(this._processorProcesses[event.id]);
+        _dismiss(JSON.stringify(event));
+    },
+
+    //used by js to interact with c++ part
+    _processConsumerEvent: function(event,topic){
+        event = JSON.parse(event);
+        for(var i=0;i<this._consumerCallbacks.length;i++){
+            if(topic.match(this._consumerCallbacks[i].topic)){
+                this._consumerCallbacks[i].callback(event);
+            }
+        }
+    },
+
+    _processProcessorEvent: function(event,topic){
+        event = JSON.parse(event); 
+        if(this._processed.indexOf(event.id) !== -1){
+            _ack(JSON.stringify(event));
+            return;
+        }
+        this._processed.push(event.id);
+        if(this._processed.length > 64){
+            this._processed.splice(0,1);
+        }
+        Duktape.fin(event,function(event){ 
+            susi.ack(event); 
+        });
+        var process = {
+            processors: [],
+            next: 0
+        };
+
+        for (var i = 0; i<this._processorCallbacks.length; i++) {
+            if(event.topic.match(this._processorCallbacks[i].topic)){
+                process.processors.push(this._processorCallbacks[i].callback);
+            } 
+        }
+
+        this._processorProcesses[event.id] = process;
+        this.ack(event);
+        Duktape.gc(); 
+    },
+
+    _processAck: function(event){ 
+        event = JSON.parse(event); 
+        var cb = this._publishCallbacks[event.id]; 
+        if(cb !== undefined) { 
+            cb(event); 
+            delete this._publishCallbacks[event.id]; 
+        }
+    },
+
+    _genID: function(){ 
+        return Math.floor(Math.random()*1000000000000); 
+    } 
+}; 
+
+//called by c++ part
+function _processConsumerEvent(event,topic){ 
+    susi._processConsumerEvent(event,topic); 
+}
+
+function _processProcessorEvent(event,topic){ 
+    susi._processProcessorEvent(event,topic) 
+}
+
+function _processAck(event){ 
+    susi._processAck(event); 
+}
+
+)SUSIJS";
