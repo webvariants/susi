59d3088fbdd7f2511d0dee9aab3b4927616c4c69
Tino Rusch <tino.rusch@webvariants.de>
2015-10-07 13:54:26 +0200

    [authenticator] progress (pattern/payload matching with regex etc.);

--- susi-authenticator/sources/Authenticator.cpp
+++ susi-authenticator/sources/Authenticator.cpp
@@ -5,8 +5,8 @@ Authenticator::Authenticator(std::string addr,short port, std::string key, std::
       load();
 }
 
-void Authenticator::addUser(User user){
-    usersByName[user.name] = user;
+void Authenticator::addUser(std::shared_ptr<User> user){
+    usersByName[user->name] = user;
 }
 
 void Authenticator::addPermission(Permission permission){
@@ -20,13 +20,13 @@ void Authenticator::load(){
         auto & users = event->payload["value"];
         for(size_t i=0;i<users.size();i++){
             auto & user = users[i];
-            User u = {
+            auto u = std::shared_ptr<User>{ new User{
                 .name = user["name"].getString(),
                 .pwHash = user["pwHash"].getString(),
                 .token = ""
-            };
+            }};
             for(size_t j=0;j<user["roles"].size();j++){
-                u.roles.push_back(user["roles"][j].getString());
+                u->roles.push_back(user["roles"][j].getString());
             }
             addUser(u);
         }
@@ -53,11 +53,11 @@ void Authenticator::save(){
     for(auto & kv : usersByName){
         auto & user = kv.second;
         userArray.push_back(BSON::Object{
-            {"name",user.name},
-            {"pwHash",user.pwHash},
+            {"name",user->name},
+            {"pwHash",user->pwHash},
             {"roles",BSON::Array{
-                user.roles.begin(),
-                user.roles.end()
+                user->roles.begin(),
+                user->roles.end()
             }}
         });
     }
@@ -125,29 +125,87 @@ void Authenticator::login(Susi::EventPtr event){
         throw std::runtime_error{"no such user"};
     }
     auto & user = usersByName[name];
-    if(pwHash != user.pwHash){
+    if(pwHash != user->pwHash){
         std::this_thread::sleep_for(std::chrono::milliseconds{100}); //this is agains brute forcers
         throw std::runtime_error{"wrong password"};
     }
-    user.token = generateToken();
-    event->payload["token"] = user.token;
+    user->token = generateToken();
+    event->payload["token"] = user->token;
+    usersByToken[user->token] = user;
 
     // prevent arbitary consumers from reading sensitive data
-    event->payload["username"] = BSON::Value{};
-    event->payload["password"] = BSON::Value{};
-
+    event->headers.push_back({"Event-Control","No-Consumer"});
     susi_->ack(std::move(event));
 }
 void Authenticator::logout(Susi::EventPtr event){
     std::string token = getTokenFromEvent(event);
-    for(auto & kv : usersByName){
-        auto & user = kv.second;
-        if(token == user.token){
-            user.token = "";
-        }
-    }
+    auto & user = usersByToken[token];
+    user->token = "";
+    usersByToken.erase(user->token);
+    event->headers.push_back({"Event-Control","No-Consumer"});
     susi_->ack(std::move(event));
 }
-void Authenticator::registerGuard(std::string topic){
 
+
+bool Authenticator::checkIfPayloadMatchesPattern(BSON::Value pattern, BSON::Value payload){
+    if(pattern.getType() != payload.getType()){
+        return false;
+    }
+    switch(pattern.getType()){
+        case BSON::UNDEFINED:
+        case BSON::INT32:
+        case BSON::INT64:
+        case BSON::DOUBLE:
+        case BSON::BOOL:
+        case BSON::DATETIME: {
+            return pattern == payload;
+        }
+        case BSON::STRING:
+        case BSON::BINARY: {
+            std::regex r{pattern.getString()};
+            return std::regex_match(payload.getString(),r);
+        }
+        case BSON::OBJECT: {
+            for(auto & kv : pattern.getObject()){
+                if(!checkIfPayloadMatchesPattern(kv.second,payload[kv.first])){
+                    return false;
+                }
+            }
+            return true;
+        }
+        case BSON::ARRAY: {
+            if(pattern.size() != payload.size()){
+                return false;
+            }
+            for(size_t i=0;i<pattern.size();i++){
+                if(!checkIfPayloadMatchesPattern(pattern[i],payload[i])){
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+    return false;
+}
+
+void Authenticator::registerGuard(Permission permission){
+    susi_->registerProcessor(permission.pattern.topic,[permission,this](Susi::EventPtr event){
+        if(checkIfPayloadMatchesPattern(permission.pattern.payload,event->payload)){
+            std::string token = getTokenFromEvent(event);
+            if(!usersByToken.count(token)){
+                susi_->dismiss(std::move(event));
+            }else{
+                auto & u = usersByToken[token];
+                for(auto & userRole : u->roles){
+                    for(auto & permissionRole: permission.roles){
+                        if(userRole == permissionRole){
+                            susi_->ack(std::move(event));
+                            return;
+                        }
+                    }
+                }
+                susi_->dismiss(std::move(event));
+            }
+        }
+    });
 }
