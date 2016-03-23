68d2e73bd287d8e456069a5a4811116a6e8ed137
christian <christian@enterprise>
2016-03-23 12:52:35 +0100

    Completed mongodb component;

--- components/susi-mongodb/headers/susi/MongoDBComponent.h
+++ components/susi-mongodb/headers/susi/MongoDBComponent.h
@@ -6,12 +6,20 @@
 #include <mongocxx/client.hpp>
 #include <mongocxx/instance.hpp>
 
+#include <vector>
+
 namespace Susi {
 
     class MongoDBComponent {
 
     public:
         MongoDBComponent(Susi::SusiClient & susi, const BSON::Value & config);
+
+        void create(const std::string collection, const BSON::Value data);
+        std::vector<BSON::Value>  find(const std::string collection, const BSON::Value query);
+        void update(const std::string collection, const BSON::Value findQuery, const BSON::Value updateQuery);
+        void remove(const std::string collection, const BSON::Value queryRemove);
+
         ~MongoDBComponent();
         void join();
 
@@ -20,6 +28,11 @@ namespace Susi {
         const BSON::Value & _config;
         mongocxx::instance inst{};
         mongocxx::client conn{mongocxx::uri{}};
+        std::string host     ;
+        std::string port     ;
+        std::string username ;
+        std::string password ;
+        std::string database ;
     };
 
 }
--- components/susi-mongodb/sources/MongoDBComponent.cpp
+++ components/susi-mongodb/sources/MongoDBComponent.cpp
@@ -2,23 +2,84 @@
 
 Susi::MongoDBComponent::MongoDBComponent(Susi::SusiClient & susi, const BSON::Value & config) : _susi{susi}, _config{config} {
 
-    bsoncxx::builder::stream::document document{};
+  try{
+    host     = _config["host"].getString();
+    port     = _config["port"].getString();
+    username = _config["username"].getString();
+    password = _config["password"].getString();
+    database = _config["database"].getString();
+  }
+  catch(const std::exception & e) {
+    std::cout << e.what() << std::endl;
+  }
 
-    auto collection = conn["testdb"]["testcollection"];
-    document << "hello" << "world";
+  _susi.registerProcessor("mongodb::create", [this](Susi::EventPtr event){
+    auto collection = event->payload["collection"].getString();
+    auto data = event->payload["data"];
+    create(collection, data);
+    event->payload["success"] = true;
+  });
 
-    collection.insert_one(document.view());
-    auto cursor = collection.find({});
+  _susi.registerProcessor("mongodb::find", [this](Susi::EventPtr event){
+    auto collection = event->payload["collection"].getString();
+    auto query = event->payload["query"];
+    event->payload["data"] = find(collection, query);
+  });
 
-    for (auto&& doc : cursor) {
-        std::cout << bsoncxx::to_json(doc) << std::endl;
-    }
+  _susi.registerProcessor("mongodb::update", [this](Susi::EventPtr event){
+    auto collection = event->payload["collection"].getString();
+    auto findQuery = event->payload["findQuery"];
+    auto updateQuery = event->payload["updateQuery"];
+    update(collection, findQuery, updateQuery);
+    event->payload["success"] = true;
+  });
+
+  _susi.registerProcessor("mongodb::remove", [this](Susi::EventPtr event){
+    auto collection = event->payload["collection"].getString();
+    auto removeQuery = event->payload["removeQuery"];
+    remove(collection, removeQuery);
+    event->payload["success"] = true;
+  });
 }
 
-Susi::MongoDBComponent::~MongoDBComponent(){
-    join();
+
+void Susi::MongoDBComponent::create(const std::string collectionName, const BSON::Value data) {
+  bsoncxx::document::value document = bsoncxx::from_json(data.toJSON());
+  auto collection = conn[database][collectionName];
+  collection.insert_one(document.view());
 }
 
-void Susi::MongoDBComponent::join(){
-    _susi.join();
+std::vector<BSON::Value> Susi::MongoDBComponent::find(const std::string collectionName, const BSON::Value query) {
+  bsoncxx::document::value document = bsoncxx::from_json(query.toJSON());
+  auto collection = conn[database][collectionName];
+  auto cursor = collection.find(document.view());
+
+  std::vector<BSON::Value> data;
+  for (auto&& doc : cursor) {
+    data.push_back(BSON::Value::fromJSON(bsoncxx::to_json(doc)));
+  }
+  return data;
+}
+
+void Susi::MongoDBComponent::update(const std::string collectionName, const BSON::Value findQuery, const BSON::Value updateQuery) {
+  bsoncxx::document::value update = bsoncxx::from_json(updateQuery.toJSON());
+  bsoncxx::document::value find   = bsoncxx::from_json(findQuery.toJSON());
+
+  auto collection = conn[database][collectionName];
+  collection.update_many(find.view(), update.view());
+}
+
+void Susi::MongoDBComponent::remove(const std::string collectionName, const BSON::Value removeQuery) {
+  bsoncxx::document::value removeMany = bsoncxx::from_json(removeQuery.toJSON());
+
+  auto collection = conn[database][collectionName];
+  collection.delete_many(removeMany.view());
+}
+
+Susi::MongoDBComponent::~MongoDBComponent() {
+  join();
+}
+
+void Susi::MongoDBComponent::join() {
+  _susi.join();
 }
