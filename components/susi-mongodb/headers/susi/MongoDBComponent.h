2a43ee11d461c66d912dcfbf6e1abe35be8b0094
Christian Bicke <christian.bicke@webvariants.de>
2016-05-27 14:05:29 +0200

    Finish mapreduce feature;

--- components/susi-mongodb/headers/susi/MongoDBComponent.h
+++ components/susi-mongodb/headers/susi/MongoDBComponent.h
@@ -22,7 +22,7 @@ namespace Susi {
 	        void update(const std::string collection, const BSON::Value findQuery, const BSON::Value updateQuery);
 	        void remove(const std::string collection, const BSON::Value queryRemove);
 			BSON::Value mapreduce(const std::string collectionName, const std::string map, const std::string reduce,
-				const BSON::Value query, const BSON::Value sort, const int limit, const std::string finalize);
+				const std::string out, const int limit);
 
 	        ~MongoDBComponent();
 	        void join();
--- components/susi-mongodb/sources/MongoDBComponent.cpp
+++ components/susi-mongodb/sources/MongoDBComponent.cpp
@@ -11,13 +11,6 @@ Susi::MongoDBComponent::MongoDBComponent(Susi::SusiClient & susi, const BSON::Va
 
 		mongoc_init();
 		client = mongoc_client_new("mongodb://127.0.0.1:27017/");
-
-		// query = BCON_NEW("cmpxchg", BCON_INT32(1));
-		// if (mongoc_collection_find_and_modify(collection, query, NULL, NULL, NULL, false, false, true, &reply, &error)) {
-		// 	std::cout << &reply << std::endl;
-		// } else {
-		// 	std::cout << &error << std::endl;
-		// }
     }
     catch (const std::exception & e) {
         std::cout << e.what() << std::endl;
@@ -59,12 +52,10 @@ Susi::MongoDBComponent::MongoDBComponent(Susi::SusiClient & susi, const BSON::Va
         auto collection = event->payload["collection"].getString();
 		auto map        = event->payload["map"].getString();
 		auto reduce     = event->payload["reduce"].getString();
-        auto query      = event->payload["query"].getObject();
-		auto sort       = event->payload["sort"].getObject();
+		auto out        = event->payload["out"].getString();
 		auto limit      = event->payload["limit"].getInt64();
-		auto finalize   = event->payload["finalize"].getString();
 
-		event->payload["data"] = mapreduce(collection, map, reduce, query, sort, limit, finalize);
+		event->payload["data"] = mapreduce(collection, map, reduce, out, limit);
     });
 }
 
@@ -104,40 +95,21 @@ void Susi::MongoDBComponent::remove(const std::string collectionName, const BSON
 
 BSON::Value Susi::MongoDBComponent::mapreduce(
 	const std::string collectionName, const std::string map, const std::string reduce,
-	const BSON::Value query, const BSON::Value sort, const int limit, const std::string finalize) {
+	const std::string out, const int limit) {
 
 	collection = mongoc_client_get_collection(client, database.c_str(), collectionName.c_str());
 
-	bson_t queryValue, sortValue;
-	bson_init_from_json(&queryValue, query.toJSON().c_str(), -1, &error);
-	bson_init_from_json(&sortValue, sort.toJSON().c_str(), -1, &error);
-
 	auto* cmd = BCON_NEW("mapReduce", collectionName.c_str());
-	bson_append_utf8(cmd, "map"     , -1, map.c_str()       , -1);
-	bson_append_utf8(cmd, "reduce"  , -1, reduce.c_str()    , -1);
-	bson_append_utf8(cmd, "out"     , -1, "function() {}"   , -1);
-	bson_append_utf8(cmd, "finalize", -1, finalize.c_str()  , -1);
-	bson_append_document(cmd, "query", -1, &queryValue);
-	bson_append_document(cmd, "sort", -1, &sortValue);
-	bson_append_int32(cmd, "limit", -1, limit);
+	bson_append_utf8 (cmd, "map"   , -1, map.c_str()   , -1);
+	bson_append_utf8 (cmd, "reduce", -1, reduce.c_str(), -1);
+	bson_append_utf8 (cmd, "out"   , -1, out.c_str()   , -1);
+	bson_append_int32(cmd, "limit" , -1, limit);
 
-// char *str = bson_as_json(cmd, NULL);
-// printf("%s\n", str);
-// bson_free(str);
-
-	if (mongoc_collection_command_simple(collection, cmd, NULL, &reply, &error)) {
-		std::cout << "Command succesful executed" << std::endl;
-	} else {
-		std::cout << "Command execution failed" << std::endl;
+	if (!mongoc_collection_command_simple(collection, cmd, NULL, &reply, &error)) {
+		throw std::runtime_error{error.message};
 	}
+
 	bson_free(cmd);
-
-// std::cout << "Command: " << bson_as_json(&cmd, NULL) << std::endl;
-// std::cout << "Answer:  " << bson_as_json(&reply, NULL) << std::endl;
-std::cout << "Domain:  " << error.domain << std::endl;
-std::cout << "Code:    " << error.code << std::endl;
-std::cout << "Message: " << error.message << std::endl;
-
 	return bson_as_json(&reply, NULL);
 }
 
